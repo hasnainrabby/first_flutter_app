4130af7046c06e4c19ed7fa24002d4a575872020
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-08-06 21:27:27 +0600

    Formatting Code.Create Directory & Import.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -15,7 +15,7 @@ class Home extends StatelessWidget //StatelessWidget is a build in class
               style: TextStyle(fontSize: 40,
                   fontStyle: FontStyle.italic,
                   fontWeight: FontWeight.bold,
-                  color: Colors.white),
+                  color: Colors.red),
             )));
     //return const Center(                                                         //Center is another widget & child is key property.
     //child: Text("Hello Flutter",textDirection: TextDirection.ltr,));
