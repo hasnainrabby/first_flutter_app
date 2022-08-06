fc05ec9723eee4ba2659570437c99d1a16c0d05a
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-08-06 21:09:37 +0600

    First Flutter app.Text widget & Material design.

---
+++ android/app/src/main/java/com/antor/app/first_flutter_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.antor.app.first_flutter_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Home());
+
+class Home extends StatelessWidget //StatelessWidget is a build in class
+{
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+        color: Colors.cyanAccent,
+        child: Center(
+            child: Text(
+          "Hello Flutter.",
+          textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 40,
+                  fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+              color: Colors.white),
+        )));
+    //return const Center(                                                         //Center is another widget & child is key property.
+    //child: Text("Hello Flutter",textDirection: TextDirection.ltr,));
+  }
+}
